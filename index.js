8756a2c9e003762d26aa1be30676f74d890b5844
taniashahida-dev <taniashahida993@gmail.com>
2026-05-07 10:42:56 +0600

    npx create-next-app@latest

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://simpleCrudUser:YGpIMURjXXhRbMPg@tania-shahida.nhmj7ah.mongodb.net/?appName=Tania-shahida`;
+
+app.use(cors())
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send('Simple Crud App')
+})
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const run =async()=>{
+try {
+    await client.connect()
+    await client.db('admin').command({ping:1})
+     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+}finally {
+    // await client.close()
+}
+}
+run().catch(console.dir)
+
+app.listen(port,()=>{
+    console.log(`Simple crud server is rinnung from  on port ${port}`)
+})
+// mongodb+srv://simpleCrudUser:YGpIMURjXXhRbMPg@tania-shahida.nhmj7ah.mongodb.net/?appName=Tania-shahida
+// mongodb+srv://<db_username>:<db_password>@tania-shahida.nhmj7ah.mongodb.net/?appName=Tania-shahida
+
+// simpleCrudUser
+// YGpIMURjXXhRbMPg
