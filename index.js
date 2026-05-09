7f6316d04a0899d33a1cb50c23ff0f8c78ecca93
taniashahida-dev <taniashahida993@gmail.com>
2026-05-09 22:10:04 +0600

    Practise basic CDRUD operation

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://simpleCrudUser:YGpIMURjXXhRbMPg@tania-shahida.nhmj7ah.mongodb.net/?appName=Tania-shahida`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_URI;
 
 app.use(cors())
 app.use(express.json())
@@ -31,6 +32,53 @@ const result =await cursor.toArray()
 res.send(result)
 })
     
+app.get('/users/:id' ,async(req,res)=>{
+    const id = req.params.id
+    const query= {
+        _id: new ObjectId(id)
+    }
+    const result = await userCullection.findOne(query)
+    res.send(result)
+})
+app.post('/users' ,async(req,res)=>{
+    const newUser= req.body
+   console.log('new user is come',newUser)
+    const result = await userCullection.insertOne(newUser)
+    res.send(result)
+})
+app.patch('/users/:id' ,async(req,res)=>{
+    const id= req.params.id
+   console.log('new user is come',id)
+const filter={
+  _id: new ObjectId(id)
+}
+const newUser = req.body
+    const updateDocument ={
+         $set: {
+    name: newUser.name,
+    role: newUser.role
+   }
+    }
+    console.log("After update",newUser)
+    const result = await userCullection.updateOne(filter,updateDocument)
+    res.send(result)
+})
+
+
+
+app.delete('/users/:id' ,async(req,res)=>{
+    const id = req.params.id
+    const query= {
+        _id: new ObjectId(id)
+    }
+    const result = await userCullection.deleteOne(query)
+    res.send(result)
+})
+
+
+
+
+
     await client.db('admin').command({ping:1})
      console.log("Pinged your deployment. You successfully connected to MongoDB!");
 }finally {
@@ -42,8 +90,3 @@ run().catch(console.dir)
 app.listen(port,()=>{
     console.log(`Simple crud server is rinnung from  on port ${port}`)
 })
-// mongodb+srv://simpleCrudUser:YGpIMURjXXhRbMPg@tania-shahida.nhmj7ah.mongodb.net/?appName=Tania-shahida
-// mongodb+srv://<db_username>:<db_password>@tania-shahida.nhmj7ah.mongodb.net/?appName=Tania-shahida
-
-// simpleCrudUser
-// YGpIMURjXXhRbMPg
